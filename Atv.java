48789130fa9809f7c2ec951bfa90aa296fdecae8
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-06-06 16:50:38 +0000

    Atividade1

---
+++ Atv.java
@@ -0,0 +1,12 @@
+public class Atv {
+    public static void main(String[] args) {
+        
+        int segundaSemana = 5;
+        int terceiraSemana = 2;
+        int quartaSemana = 7;
+
+        System.out.println(segundaSemana);
+        System.out.println(terceiraSemana);
+        System.out.println(quartaSemana);
+    }
+}
